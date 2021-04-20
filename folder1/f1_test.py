99ed4efbd2fb3248d66875ad92ab03da1417eec0
Lubomir Bucko <lubomir.bucko@gmail.com>
2021-04-20 13:04:58 -0700

    Added testing folders #1 & #2

---
+++ folder1/f1_test.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+def testf():
+    print("testing function worki")
+testf()
---
+++ folder2/f2_test.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+var = "Monday"
+def workday(day = "Friday"):
+    print(day)
+workday(var)
